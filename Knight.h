64878a1d4ac6117b49eba5cff756129fd559bcb4
dvir <dvir.magshimim@gmail.com>
2024-12-28 12:49:12 +0200

    knightFIX

--- Knight.h
+++ Knight.h
@@ -4,5 +4,5 @@
 class Knight : public Piece
 {
 public:
-	bool checkMove(Piece board[8][8], Piece source, Piece destination) const;
+	bool checkMove(Piece* board[8][8], Piece* destination) const;
 };
