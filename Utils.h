cd601bd0bae85143841d596e12d354cb440d1e91
thekizner <thekizner@gmail.com>
2024-12-29 01:44:40 +0200

    Expanded movePiece()

--- Utils.h
+++ Utils.h
@@ -26,8 +26,23 @@ public:
 			throw std::string("ERROR: Invalid input!\n");
 		}
 	}
-	static void movePiece(Piece& source, Piece& destination, char& currentPlayer)
+	static void movePiece(Piece* board[8][8], Piece& source, Piece& destination, char& currentPlayer)
 	{
+		char pieceType = source.getType();
+		Piece* p;
+		switch (tolower(pieceType))
+		{
+		case 'b': p = new Bishop(pieceType, destination.getY(), destination.getX()); break;
+		case 'k': p = new King(pieceType, destination.getY(), destination.getX()); break;
+		case 'n': p = new Knight(pieceType, destination.getY(), destination.getX()); break;
+		case 'p': p = new Pawn(pieceType, destination.getY(), destination.getX()); break;
+		case 'q': p = new Queen(pieceType, destination.getY(), destination.getX()); break;
+		case 'r': p = new Rook(pieceType, destination.getY(), destination.getX()); break;
+		default: p = new nullPiece(pieceType, destination.getY(), destination.getX()); break;
+		}
+		board[destination.getY()][destination.getX()] = p;
+
+		board[source.getY()][source.getX()] = new nullPiece(EMPTY, source.getY(), source.getX());
 		destination.setType(source.getType());
 		destination.setColor(source.getColor());
 
