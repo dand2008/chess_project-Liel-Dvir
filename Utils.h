ac0f58899458ffc06b1279f124ad0786d084d4f9
thekizner <thekizner@gmail.com>
2024-12-30 01:19:21 +0200

    Fixed Utils

--- Utils.h
+++ Utils.h
@@ -28,6 +28,7 @@ public:
 	}
 	static void movePiece(Piece* board[8][8], Piece& source, Piece& destination, char& currentPlayer)
 	{
+
 		Piece* p = source.clone(&destination);
 
 		board[destination.getY()][destination.getX()] = p;
