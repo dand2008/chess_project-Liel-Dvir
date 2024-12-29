d230269b170cf61e74844f664b6efe674693fecd
thekizner <thekizner@gmail.com>
2024-12-30 01:32:01 +0200

    Polished Console

--- Console.cpp
+++ Console.cpp
@@ -4,6 +4,8 @@ using std::cout;
 using std::endl;
 using std::string;
 
+void processMoveCode(char code);
+
 int main()
 {
 	char setup[66];
@@ -19,27 +21,16 @@ int main()
 	while (input != "quit")
 	{
 		system("cls");
-		char *code = e.getCode(input);
-		e.displayBoard();
-		switch (code[0])
-		{
-		case '2':
-			std::cout << "ERROR: Not your piece!\n";
-			break;
-		case '3':
-			std::cout << "ERROR: Destination square cannot be your piece!\n";
-			break;
-		case '5':
-			std::cout << "ERROR: Invalid input!\n";
-			break;
-		case '6':
-			std::cout << "ERROR: Invalid move: Path obstructed or illegal move for piece.\n";
-			break;
-		case '7':
-			std::cout << "ERROR: Source and Destination squares are the same!\n";
-			break;
 
+		char *code = e.getCode(input);
+
+		if (code[0] == '0')
+		{
+			e.movePiece(input);
 		}
+		e.displayBoard();
+
+		processMoveCode(code[0]);
 
 		std::cout << "\n" << (e.getCurrPlayer() == 'w' ? "White" : "Black") << " to move." << std::endl;
 		std::cout << "Enter move (e.g. e2e4): ";
@@ -47,4 +38,26 @@ int main()
 	}
 
 	return 0;
+}
+
+void processMoveCode(char code)
+{
+	switch (code)
+	{
+	case '2':
+		std::cout << "ERROR: Not your piece!\n";
+		break;
+	case '3':
+		std::cout << "ERROR: Destination square cannot be your piece!\n";
+		break;
+	case '5':
+		std::cout << "ERROR: Invalid input!\n";
+		break;
+	case '6':
+		std::cout << "ERROR: Invalid move: Path obstructed or illegal move for piece.\n";
+		break;
+	case '7':
+		std::cout << "ERROR: Source and Destination squares are the same!\n";
+		break;
+	}
 }
